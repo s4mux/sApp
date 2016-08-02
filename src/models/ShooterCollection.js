5e2cd90ecd2af27be4fdefe7defe45f0b5e6b55a
Samuel Remy <s4mux@me.com>
2016-08-02 13:55:50 +0200

    Add Find method to collection which searches for any matching string in name, firstname and club attributes of the models

--- src/models/ShooterCollection.js
+++ src/models/ShooterCollection.js
@@ -4,5 +4,22 @@ var ShooterModel = require("./ShooterModel");
 module.exports = Backbone.Collection.extend({
     model: ShooterModel,
     url: '/shooters',
-    comparator: 'firstname'
+    comparator: 'firstname',
+
+    find: function(str){
+      str = str.toLowerCase();
+      return this.filter(function(item){
+        var props = ["name", "firstname", "club"]; //TODO: add "group"
+        if(str=="") return true; //When no filter is active we want to return the whole array
+        for(var i=0; i<props.length; i++){
+          var value = item.get(props[i]);
+          if(value){
+            if(value.toLowerCase().indexOf(str) > -1){
+              return true;
+            }
+          }
+        }
+        return false;
+      });
+    }
 });
