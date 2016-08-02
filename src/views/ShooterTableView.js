c67bd0331cabb7bcf38d4969a20f475d7fa607eb
Samuel Remy <s4mux@me.com>
2016-08-02 13:57:04 +0200

    Update ShooterTableView in order to allow filtering with the search field

--- src/views/ShooterTableView.js
+++ src/views/ShooterTableView.js
@@ -10,6 +10,7 @@ Backbone.$ = $;
 
 var views = {};
 var that;
+var textFilter = "";
 
 module.exports = Backbone.View.extend({
   //className: "list-group",
@@ -19,7 +20,8 @@ module.exports = Backbone.View.extend({
     that = this;
   },
   events: {
-    "click #addShooterButton" : "addSchooterClicked"
+    "click #addShooterButton" : "addSchooterClicked",
+    "keyup #tableSearchField" : "filter"
   },
   addOneItem: function(model){
     views[model.id] = new ShooterItemView({model: model});
@@ -27,14 +29,28 @@ module.exports = Backbone.View.extend({
   },
 
   render: function () {
-    this.$el.html(template({participants: "Schützen", name: "Name", club: "Verein", group: "Gruppe", searchPlaceholder: "Suche..."}));
-    _.forEach(this.collection.models, this.addOneItem, this);
+    this.$el.html(template({participants: "Schützen", name: "Name", club: "Verein", group: "Gruppe", search: textFilter, searchPlaceholder: "Suche..."}));
+    _.forEach(this.collection.find(textFilter), this.addOneItem, this);
     return this;
   },
+
+  updateList: function(filter){
+    textFilter = filter;
+    _.forEach(views, function(view){view.remove()});
+    _.forEach(this.collection.find(textFilter), this.addOneItem, this);
+  },
+
 
   highlight: function(id){
       that.$el.find("a").removeClass("active");
       views[id].$el.addClass("active");
+  },
+
+  filter: function(event){
+    event.preventDefault();
+    console.log(event.target.value);
+    this.updateList(event.target.value);
+
   },
 
   addSchooterClicked: function(event){
